66c73c34d8c1085ad3b1702cee441e66918cf839
Herve Iuliano Natama Purba <herve.inp@outlook.com>
2026-09-25 17:42:50 +0700

    refactor(day-01): update main.py and README.md

--- day-01-band-name-generator/main.py
+++ day-01-band-name-generator/main.py
@@ -1,5 +1,18 @@
+"""
+Band Name Generator
+A simple CLI application that combines a city name and a pet's name
+to generate a creative band name.
+"""
+
+# Display welcome message
 print("Welcome to the Band Name Generator.")
+
+# 1. Collect inputs from the user
 city = input("What's the name of the city you grew up in?\n")
 pet = input("What's your pet's name?\n")
+
+# 2. Combine inputs using string concatenate to generate the band name
 band_name = city + " " + pet
+
+# 3. Display the generated band name
 print("Your band name could be " + band_name)
